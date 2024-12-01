eae37db9971ef5b03118451b023b49d7377d1b71
softbf395 <130502864+softbf395@users.noreply.github.com>
2024-11-30 19:07:22 -0600

    Update Titan TV man.lua

--- Titan TV man.lua
+++ Titan TV man.lua
@@ -1,8 +1,5 @@
 function notif(title, desc)
-  game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = title or "Error",
-	Text = desc or "Error"
-})
+  game:GetService("StarterGui"):SetCore("SendNotification",{Title = title or "Error", Text = desc or "Error"})
 end
 if not game.Players.LocalPlayer.Backpack:FindFirstChild("Normal Punch") then
 	notif("Unable to start:", "Either holding a saitama move or arent saitama")
@@ -25,7 +22,7 @@ local ultText=game.Players.LocalPlayer.PlayerGui.ScreenGui.MagicHealth.TextLabel
 local ultBar=ultText.Parent.Health.Bar
 ultText.Text="Rage Mode"
 ultBar.ImageColor3=Color3.new(1,0,1)
-local tools=game.Player.LocalPlayer.Backpack
+local tools=game.Players.LocalPlayer.Backpack
 local move1=tools["Normal Punch"]
 local move2=tools["Consecutive Punches"]
 local move3=tools["Shove"]
@@ -33,5 +30,5 @@ local move4=tools["Uppercut"]
 local hotbar=game.Players.LocalPlayer.PlayerGui.Hotbar.Backpack.Hotbar
 hotbar["1"].Base.ToolName.Text="RSOD"
 hotbar["2"].Base.ToolName.Text="Energy sword 1"
-hotbar["3"].Base.ToolName.Text="Energy sword 2
+hotbar["3"].Base.ToolName.Text="Energy sword 2"
 hotbar["4"].Base.ToolName.Text="Core blast"
